c56242c0b9c7b7d25d32603471fd9f0a80331280
Alexander M Piper <alexpiperdesigns@gmail.com>
2019-02-20 17:24:02 +1100

    Update markdown
    Added correlation plots

--- demulti/all_combinations.R
+++ demulti/all_combinations.R
@@ -3,7 +3,7 @@ library(viridis)
 ##CREATE ALL COMBINATIONS
 
 #Read in original sample sheet
-SampleSheet <- read_csv("demulti/SampleSheet_Run6.csv",skip=20)
+SampleSheet <- read_csv("demulti/SampleSheet.csv",skip=20)
 
 I7_Index_ID <- SampleSheet$index
 I5_Index_ID <- SampleSheet$index2
@@ -19,11 +19,11 @@ real$name <- paste0(real$I7_Index_ID,"_",real$I5_Index_ID)
 
 unexpected <- combos[!combos$name %in% real$name, ]
 
-write.csv(unexpected,file="demulti/Run6_unexpected.csv")
+#write.csv(unexpected,file="demulti/Run5_unexpected.csv")
 
 ##READ DEMULTIPLEXED ALL
 
-demulti <- read.table("demulti/readcount_mock.txt")
+demulti <- read.table("demulti/readcount_250.txt")
 
 unique_sample <- demulti$V1 %>%
   str_replace_all("-","_") %>%
@@ -47,14 +47,25 @@ demulti <- left_join(demulti,real,by="sample")
 
 demulti$name[is.na(demulti$name)] <- demulti$sample[is.na(demulti$name)]
 
-demulti <- demulti %>%
+##Summary of index switching rate
+exp_rate <- demulti %>% 
+  filter(str_detect(sample,paste(c("Pool", "Trap"),collapse = '|')))
+obs_rate <- demulti %>% 
+  filter(!str_detect(sample,paste(c("Pool", "Trap"),collapse = '|')))
+
+switch_rate <- (sum(obs_rate$reads)/sum(exp_rate$reads)) *100
+
+##plot index switching
+
+plot <- demulti %>%
   separate(name,c("i7","i5"),"_")%>%
   select(i7,i5,reads)
 
-p1 <- ggplot(data = demulti, aes(x = i7, y = i5), stat="identity") +
+p1 <- ggplot(data = plot, aes(x = i7, y = i5), stat="identity") +
   geom_tile(aes(fill = reads))  + scale_fill_viridis(name = "reads", trans = "log10")  + geom_text(label=demulti$reads) +
   scale_x_discrete(limits=SampleSheet$index,expand=c(0,0))+ scale_y_discrete(limits=SampleSheet$index2,expand=c(0,0)) + 
-  theme(axis.text.x = element_text(angle=90, hjust=1), plot.title=element_text(hjust = 0.5), legend.position = "bottom") +labs(title= "100-500-1000 Mock Samples")
+  theme(axis.text.x = element_text(angle=90, hjust=1), plot.title=element_text(hjust = 0.5), plot.subtitle =element_text(hjust = 0.5), legend.position = "bottom") +
+  labs(title= "Trap Samples", subtitle = paste0("Index switch rate for the run: ", sprintf("%1.2f%%", switch_rate)))
 
 
 ##Subset to C5
@@ -62,7 +73,7 @@ p1 <- ggplot(data = demulti, aes(x = i7, y = i5), stat="identity") +
 combinatorial <- SampleSheet[1:5,]
 unique <- SampleSheet[6:10,]
 
-c5 <- demulti[demulti$i5 %in% combinatorial$index2 & demulti$i7 %in% combinatorial$index, ]
+c5 <- plot[plot$i5 %in% combinatorial$index2 & plot$i7 %in% combinatorial$index, ]
 
 c5plot <- ggplot(data = c5, aes(x = i7, y = i5), stat="identity") +
   geom_tile(aes(fill = reads)) + scale_fill_viridis(name = "reads", trans = "log10") + geom_text(label=c5$reads)+
@@ -70,13 +81,23 @@ c5plot <- ggplot(data = c5, aes(x = i7, y = i5), stat="identity") +
   theme(axis.text.x = element_text(angle=90, hjust=1), plot.title=element_text(hjust = 0.5), legend.position = "bottom") +
   labs(title= "Combinatorial Indexing")
 
-u5 <- demulti[demulti$i5 %in% unique$index2 & demulti$i7 %in% unique$index, ]
+u5 <- plot[plot$i5 %in% unique$index2 & plot$i7 %in% unique$index, ]
 
 
 u5plot <-  ggplot(data = u5, aes(x = i7, y = i5), stat="identity", main="Unique-Dual Indexing") +
   geom_tile(aes(fill = reads)) + scale_fill_viridis(name = "reads", trans = "log10")  + geom_text(label=u5$reads) +
   scale_x_discrete(limits=unique$index,expand=c(0,0))+ scale_y_discrete(limits=unique$index2,expand=c(0,0)) + 
   theme(axis.text.x = element_text(angle=90, hjust=1), plot.title=element_text(hjust = 0.5), legend.position = "bottom") +labs(title= "Unique-Dual Indexing")
+
+
+##Summary of index switching rate - U5
+u5_exp <- u5 %>% 
+  filter( reads > 100000)
+u5_obs<- u5 %>% 
+  filter( reads < 100000)
+
+switch_rate <- (sum(u5_obs$reads)/sum(u5_exp$reads)) *100
+
 
 ggarrange(c5plot,u5plot, ncol=2,nrow=1, common.legend = TRUE, legend="bottom")
 
